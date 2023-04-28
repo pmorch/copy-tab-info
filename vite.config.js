559b27863bc7b68d22ef9706543500e744b49492
Peter Valdemar Mørch <peter@morch.com>
2023-04-28 15:38:40 +0200

    only build minimal options.html entry

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { resolve } from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -10,5 +12,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        options: resolve(__dirname, 'src', 'options', 'options.html')
+      },
+    },
   }
 })
